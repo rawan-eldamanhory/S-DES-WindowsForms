4d5391d03bfabef200a80f6b48dd49e1dc401db1
Rawan Eldamanhory <rawaneldamanhory@gmail.com>
2026-01-19 19:54:59 +0200

    Initial commit: Educational S-DES encryption demo using Windows Forms

---
+++ WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,106 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtDecrypted = new System.Windows.Forms.TextBox();
+            this.btnDecrypt = new System.Windows.Forms.Button();
+            this.btnEncrypt = new System.Windows.Forms.Button();
+            this.txtEncrypted = new System.Windows.Forms.TextBox();
+            this.txtPlaintext = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // txtDecrypted
+            // 
+            this.txtDecrypted.Location = new System.Drawing.Point(616, 117);
+            this.txtDecrypted.Name = "txtDecrypted";
+            this.txtDecrypted.Size = new System.Drawing.Size(100, 22);
+            this.txtDecrypted.TabIndex = 10;
+            // 
+            // btnDecrypt
+            // 
+            this.btnDecrypt.Location = new System.Drawing.Point(483, 117);
+            this.btnDecrypt.Name = "btnDecrypt";
+            this.btnDecrypt.Size = new System.Drawing.Size(75, 23);
+            this.btnDecrypt.TabIndex = 9;
+            this.btnDecrypt.Text = "decrypt";
+            this.btnDecrypt.UseVisualStyleBackColor = true;
+            this.btnDecrypt.Click += new System.EventHandler(this.BtnDecrypt_Click);
+            // 
+            // btnEncrypt
+            // 
+            this.btnEncrypt.Location = new System.Drawing.Point(228, 117);
+            this.btnEncrypt.Name = "btnEncrypt";
+            this.btnEncrypt.Size = new System.Drawing.Size(75, 23);
+            this.btnEncrypt.TabIndex = 8;
+            this.btnEncrypt.Text = "encrypt";
+            this.btnEncrypt.UseVisualStyleBackColor = true;
+            this.btnEncrypt.Click += new System.EventHandler(this.BtnEncrypt_Click);
+            // 
+            // txtEncrypted
+            // 
+            this.txtEncrypted.Location = new System.Drawing.Point(355, 117);
+            this.txtEncrypted.Name = "txtEncrypted";
+            this.txtEncrypted.Size = new System.Drawing.Size(100, 22);
+            this.txtEncrypted.TabIndex = 7;
+            // 
+            // txtPlaintext
+            // 
+            this.txtPlaintext.Location = new System.Drawing.Point(78, 117);
+            this.txtPlaintext.Name = "txtPlaintext";
+            this.txtPlaintext.Size = new System.Drawing.Size(100, 22);
+            this.txtPlaintext.TabIndex = 6;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.txtDecrypted);
+            this.Controls.Add(this.btnDecrypt);
+            this.Controls.Add(this.btnEncrypt);
+            this.Controls.Add(this.txtEncrypted);
+            this.Controls.Add(this.txtPlaintext);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtDecrypted;
+        private System.Windows.Forms.Button btnDecrypt;
+        private System.Windows.Forms.Button btnEncrypt;
+        private System.Windows.Forms.TextBox txtEncrypted;
+        private System.Windows.Forms.TextBox txtPlaintext;
+    }
+}
+
---
+++ WindowsFormsApp1/Form1.cs
@@ -0,0 +1,223 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        private byte[] sharedKey;
+
+        // S-DES Constants
+        private static readonly int[] IP = { 1, 5, 2, 0, 3, 7, 4, 6 };
+        private static readonly int[] FP = { 3, 0, 2, 4, 6, 1, 7, 5 };
+        private static readonly int[] EP = { 3, 0, 1, 2, 1, 2, 3, 0 };
+        private static readonly int[,] S0 = {
+            { 1, 0, 3, 2 },
+            { 3, 2, 1, 0 },
+            { 0, 2, 1, 3 },
+            { 3, 1, 3, 2 }
+        };
+        private static readonly int[,] S1 = {
+            { 0, 1, 2, 3 },
+            { 2, 0, 1, 3 },
+            { 3, 0, 1, 0 },
+            { 2, 1, 0, 3 }
+        };
+        private static readonly int[] P4 = { 1, 3, 2, 0 };
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // Step 1: Perform the Diffie-Hellman key exchange protocol
+            using (var dh = new ECDiffieHellmanCng())
+            {
+                dh.KeyDerivationFunction = ECDiffieHellmanKeyDerivationFunction.Hash;
+                dh.HashAlgorithm = CngAlgorithm.Sha256;
+
+                // Generate your private key
+                byte[] privateKey = dh.Key.Export(CngKeyBlobFormat.EccPrivateBlob);
+
+                // Step 2: Share your public key with the other party
+                byte[] publicKey = dh.PublicKey.ToByteArray();
+
+                // Step 3: Use your public key as the other party's public key (self-loop)
+                byte[] otherPartyPublicKey = publicKey;
+
+                // Step 4: Derive the shared secret key
+                sharedKey = dh.DeriveKeyMaterial(CngKey.Import(otherPartyPublicKey, CngKeyBlobFormat.EccPublicBlob));
+
+                // The sharedKey is now ready for encryption and decryption
+            }
+        }
+
+        private void BtnEncrypt_Click(object sender, EventArgs e)
+        {
+            if (string.IsNullOrWhiteSpace(txtPlaintext.Text))
+            {
+                MessageBox.Show("Please enter the plaintext to encrypt.");
+                return;
+            }
+
+            string plaintext = txtPlaintext.Text;
+
+            // Step 5: Encrypt the plaintext
+            string ciphertext = EncryptText(plaintext, sharedKey);
+
+            // Display the ciphertext
+            txtEncrypted.Text = ciphertext;
+        }
+
+        private void BtnDecrypt_Click(object sender, EventArgs e)
+        {
+            if (string.IsNullOrWhiteSpace(txtEncrypted.Text))
+            {
+                MessageBox.Show("Please enter the ciphertext to decrypt.");
+                return;
+            }
+
+            string ciphertext = txtEncrypted.Text;
+
+            // Step 6: Decrypt the ciphertext
+            string decryptedText = DecryptText(ciphertext, sharedKey);
+
+            // Display the decrypted text
+            txtDecrypted.Text = decryptedText;
+        }
+
+        private string EncryptText(string plainText, byte[] key)
+        {
+            byte[] plaintextBytes = Encoding.ASCII.GetBytes(plainText);
+            byte[] encryptedBytes = new byte[plaintextBytes.Length];
+
+            for (int i = 0; i < plaintextBytes.Length; i++)
+            {
+                byte encryptedByte = EncryptByte(plaintextBytes[i], key);
+                encryptedBytes[i] = encryptedByte;
+            }
+
+            return Convert.ToBase64String(encryptedBytes);
+        }
+
+        private string DecryptText(string ciphertext, byte[] key)
+        {
+            byte[] encryptedBytes = Convert.FromBase64String(ciphertext);
+            byte[] decryptedBytes = new byte[encryptedBytes.Length];
+
+            for (int i = 0; i < encryptedBytes.Length; i++)
+            {
+                byte decryptedByte = DecryptByte(encryptedBytes[i], key);
+                decryptedBytes[i] = decryptedByte;
+            }
+
+            return Encoding.ASCII.GetString(decryptedBytes);
+        }
+
+        private byte EncryptByte(byte plaintextByte, byte[] key)
+        {
+            byte[] subkeys = GenerateSubkeys(key);
+
+            byte initialPermutation = Permute(plaintextByte, IP);
+
+            byte fkResult = ApplyFk(initialPermutation, subkeys[0]);
+            byte middleStep = ((byte)(fkResult << 4 | fkResult >> 4));
+
+            byte swResult = ApplySw(middleStep);
+
+            byte encryptedByte = Permute(swResult, FP);
+
+            return encryptedByte;
+        }
+
+        private byte DecryptByte(byte encryptedByte, byte[] key)
+        {
+            byte[] subkeys = GenerateSubkeys(key);
+
+            byte initialPermutation = Permute(encryptedByte, IP);
+
+            byte fkResult = ApplyFk(initialPermutation, subkeys[1]);
+
+            byte middleStep = ((byte)(fkResult << 4 | fkResult >> 4));
+
+            byte swResult = ApplySw(middleStep);
+
+            byte decryptedByte = Permute(swResult, FP);
+
+            return decryptedByte;
+        }
+
+        private byte ApplyFk(byte value, byte subkey)
+        {
+            byte expandedValue = Permute(value, EP);
+
+            byte xorResult = ((byte)(expandedValue ^ subkey));
+
+            byte left4bits = (byte)(xorResult >> 4);
+            byte right4bits = (byte)(xorResult & 0x0F);
+
+            byte s0Result = (byte)S0[left4bits >> 2, left4bits & 0x03];
+            byte s1Result = (byte)S1[right4bits >> 2, right4bits & 0x03];
+
+            byte mergedResult = (byte)(s0Result << 2 | s1Result);
+
+            byte p4Result = Permute(mergedResult, P4);
+
+            byte fkResult = ((byte)(value ^ p4Result));
+
+            return fkResult;
+        }
+
+        private byte ApplySw(byte value)
+        {
+            byte leftHalf = (byte)(value >> 4);
+            byte rightHalf = (byte)(value & 0x0F);
+
+            byte swResult = (byte)(rightHalf << 4 | leftHalf);
+
+            return swResult;
+        }
+
+        private byte[] GenerateSubkeys(byte[] key)
+        {
+            byte[] subkeys = new byte[2];
+
+            byte p10Result = Permute(key[0], new int[] { 2, 4, 1, 6, 3, 9, 0, 8, 7, 5 });
+            byte p8Result = Permute(p10Result, new int[] { 5, 2, 6, 3, 7, 4, 9, 8 });
+
+            subkeys[0] = p8Result;
+
+            byte leftShiftedKey = (byte)((key[0] << 1) | (key[1] >> 3));
+            byte p10Result2 = Permute(leftShiftedKey, new int[] { 2, 4, 1, 6, 3, 9, 0, 8, 7, 5 });
+            byte p8Result2 = Permute(p10Result2, new int[] { 5, 2, 6, 3, 7, 4, 9, 8 });
+
+            subkeys[1] = p8Result2;
+
+            return subkeys;
+        }
+
+        private byte Permute(byte value, int[] permutation)
+        {
+            byte result = 0;
+
+            for (int i = 0; i < permutation.Length; i++)
+            {
+                int bitPosition = permutation[i];
+                byte bitValue = (byte)((value >> (7 - bitPosition)) & 0x01);
+                result |= (byte)(bitValue << (permutation.Length - 1 - i));
+            }
+
+            return result;
+        }
+    }
+}
